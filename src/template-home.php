4dc444663ae2139a5e8983db85c4bbf507bb0a5a
Marko <msustic91@gmail.com>
2018-03-10 17:52:44 +0100

    Finished black and white sections of the home template

--- src/template-home.php
+++ src/template-home.php
@@ -11,4 +11,33 @@
 			</p>
 		</div>
 	</section>
+	<section class="home-content-section-dark">
+		<div class="home-content-sub-wrapper">
+			<h3><span>Mi smo kreativni</span> tim sačinjen od novinara, snimatelja, montažera, dizajnera, programera, naratora, spikera, producenata...</h3>
+		</div>
+		<div class="home-content-section-devider-left"></div>
+		<div class="home-content-sub-wrapper">
+			<p>Za Vaš projekt imamo sve što trebate. Uz više od <span>20 godina iskustva</span> u medijima, stotinama projekata i zadovoljnih klijenata , nekoliko internetskih portala, televizijskih i radijskih emisija posjedujemo iskustvo, kontakte, znanje i opremu da proizvedemo foto, video ili audio na svjetskoj razini.</p>
+			<p><span>Vašem projektu pristupamo profesionalno</span>, od idejne faze sve do postprodukcije i implementacije. Zajedno s Vama radimo na ostvarenju Vaših želja, ideja, ciljeva i vizija.</p>
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/services-infographics.svg" alt="Tomich Productions Services infographics. Listing all services. Video editing, autio production, audio editing, video production, animation, pro forography copywriting.">
+		</div>
+	</section>
+	<section class="home-content-section-light">
+		<div class="home-content-sub-wrapper">
+			<h3>Klijenti koji su nam iskazali povjerenje.</h3>
+		</div>
+		<div class="home-content-section-devider-left"></div>
+		<div class="home-content-sub-wrapper">
+			<p>Postanite i Vi jedan od zadovoljnih klijenata.</p>
+			<a href="#" class="blue-cta cta-button" title="">KONTAKTIRAJ NAS</a>
+		</div>
+		<div class="client-logos">
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/clients/combis.jpg">
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/clients/coopans-logo.jpg">
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/clients/ibm.jpg">
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/clients/ictbusinesstv.jpg">
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/clients/oracle.jpg">
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/clients/zracna_plovidba.jpg">
+		</div>
+	</section>
 <?php get_footer(); ?>
